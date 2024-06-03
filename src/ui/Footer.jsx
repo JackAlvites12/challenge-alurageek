928cd8483e9029ca94982150e6d41a2da5b2b240
JackAlvites12 <jack_manuel_alvites_12@hotmail.com>
2024-06-03 17:12:11 -0500

    update UI

--- src/ui/Footer.jsx
+++ src/ui/Footer.jsx
@@ -7,7 +7,7 @@ export const Footer = () => {
 
         <div className="footer__content">
             <figure className="header__figure">
-                <img src="/challenge-alurageek/assets/hearths.svg" alt="hearths" />
+                <img src="https://firebasestorage.googleapis.com/v0/b/react-firebase-alurageek.appspot.com/o/hearths.svg?alt=media&token=af733981-6e2f-4047-ba76-b648dcbd4310" alt="hearths" />
             </figure>
 
             <h3 className="footer__title">AluraGeek</h3>
--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -4,7 +4,7 @@ export const Header = () => {
 
     <header className="header">
         <figure className="header__figure">
-            <img src="/challenge-alurageek/assets/hearths.svg" alt="hearths" />
+            <img src="https://firebasestorage.googleapis.com/v0/b/react-firebase-alurageek.appspot.com/o/hearths.svg?alt=media&token=af733981-6e2f-4047-ba76-b648dcbd4310" alt="hearths" />
         </figure>
         <h3 className="header__title">AluraGeek</h3>
     </header>
